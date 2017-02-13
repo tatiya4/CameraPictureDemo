a04311f4f121b5465054af2737a1bf5c6ad11e58
tatiya4 <tatiya.prashant@gmail.com>
2017-02-13 15:13:58 +0530

    I am Adding comment on this file

--- app/src/main/java/com/example/firebase/camerapicturedemo/MainActivity.java
+++ app/src/main/java/com/example/firebase/camerapicturedemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //My First Example
+        //My First Example This Is Demo
     }
 }
